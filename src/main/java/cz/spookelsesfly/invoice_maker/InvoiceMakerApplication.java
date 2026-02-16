d8ca2eb4e5ba3ca3e2249ef48b9b1907fce1f3e5
Aleksandr Galaev <ag.aleksandr.galaev@gmail.com>
2026-02-16 10:46:26 +0100

    Initial project setup, configure project parameters, add Maven dependencies, configure JPA and Postgres

---
+++ src/main/java/cz/spookelsesfly/invoice_maker/InvoiceMakerApplication.java
@@ -0,0 +1,12 @@
+package cz.spookelsesfly.invoice_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceMakerApplication.class, args);
+	}
+}
